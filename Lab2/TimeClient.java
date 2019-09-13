c132401ce7b3d4fefdfb217f5d45713fddb8c656
unknown <seay.johnny@gmail.com>
2019-09-13 12:42:40 -0500

    lab 3

--- Lab2/TimeClient.java
+++ Lab2/TimeClient.java
@@ -1,23 +1,33 @@
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 
-public class TimeClient {
+public class ThreadedTimeClient {
 
     public static void main(String[] args) {
-        if (args.length < 2) return;
+        if (args.length < 4) return;
 
         String hostname = args[0];
         int port = Integer.parseInt(args[1]);
+        String response;
 
         try (Socket socket = new Socket(hostname, port)) {
             InputStream input = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
-            String time = reader.readLine();
-            System.out.println(time);
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            writer.println(args[2] + " " + args[3]);
+            response = reader.readLine();
+            do
+            {
+                if(!response.isEmpty()){
+                    System.out.println(response);
+                }
+                response = reader.readLine();
+            } while (response.isEmpty());
+
         }
         catch (Exception e) {
             System.out.println("Client Exception: " + e.getMessage());
